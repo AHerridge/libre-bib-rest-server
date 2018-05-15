4c352cc07f81c35b092f6b1a5398c9da72cebd94
Alex Herridge <alherridge@gmail.com>
2018-05-15 19:38:00 -0400

    Added @Repository to ContractRepository

--- src/main/java/com/aherridge/librebib/server/contract/ContractRepository.java
+++ src/main/java/com/aherridge/librebib/server/contract/ContractRepository.java
@@ -3,7 +3,9 @@ package com.aherridge.librebib.server.contract;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
+@Repository
 @RepositoryRestResource(collectionResourceRel = "contracts", path = "contracts")
 public interface ContractRepository extends PagingAndSortingRepository<Contract, String> {
 
